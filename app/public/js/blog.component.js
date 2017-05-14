861e662f1c1d22552a2a351da99e368aeb62bdcf
sagerioz <ericaepperson27@yahoo.com>
2017-05-14 10:05:44 -0600

    comments behaving themselves

--- app/public/js/blog.component.js
+++ app/public/js/blog.component.js
@@ -3,72 +3,76 @@
 
   angular.module('app')
     .component('blog', {
-    controller: controller,
-     templateUrl: './template/template.html'
-   })
+      controller: controller,
+      templateUrl: './template/template.html'
+    })
 
-controller.$inject = ['blogService']
-function controller(blogService) {
-const vm = this
-console.log("VM", vm);
+  controller.$inject = ['blogService']
+
+  function controller(blogService, $stateParams) {
+    const vm = this;
+    console.log("VM", vm);
+    vm.time = new Date();
 
 
-vm.$onInit = function () {
-   console.log("hi");
-  vm.formVisible = false;
-  vm.formNotVisible = true;
-  vm.commentsVisible = true;
-  // vm.postObj = {};
-  blogService.getBlog().then(function(data){
-    console.log("DATA", data);
-    vm.blogDb = data
-    console.log("BLOGDB", vm.blogDb);
+    vm.toggleForm = function() {
+      vm.formVisible = !vm.formVisible
+      vm.formNotVisible = !vm.formNotVisible
+    };
 
-  })
+    vm.$onInit = function() {
+      vm.formVisible = false
+      vm.formNotVisible = true
 
-  vm.toggleForm = function () {
-     vm.formVisible = !vm.formVisible
-     vm.formNotVisible = !vm.formNotVisible
+      blogService.getBlog().then(function(data) {
+        console.log("DATA", data);
+        vm.blogDb = data
+        console.log("BLOGDB", vm.blogDb);
+      });
+    }
+
+    vm.createPost = function() {
+      let tempObj = {
+        title: vm.postObj.title,
+        body: vm.postObj.body,
+        author: vm.postObj.author,
+        image_url: vm.postObj.image_url,
+        vote_count: 0,
+        created_at: moment().calendar()
+      }
+      blogService.sendForm(tempObj).then(function(data) {
+        console.log("DATA after new Form", data);
+        data.comments = []
+        vm.blogDb.push(data)
+        console.log("COMMENTS in DATA?", data);
+        delete vm.postObj;
+        vm.toggleForm()
+      })
+    }
+    vm.deletePosts = function(posts) {
+      blogService.deletePost(posts).then(function(data) {
+        console.log("you deleted me");
+      });
+    }
+
+    console.log("LINE 57");
+    vm.toggleComments = function(posts) {
+      posts.commentsVisible = !posts.commentsVisible
+      console.log("WHATS THIS", posts.commentsVisible);
+    };
+
+    // blogService.countVotesUp(posts).then(function(data) {
+    //   console.log("UP COUNT IN COMPONENT FUNCTION", data);
+    //   console.log("POST COUNT IN COMPONENT FUNCTION", posts.vote_count);
+    //   posts.vote_count += 1
+    // });
+
+    vm.countVotesDown = function(posts) {
+      let temp = posts.vote_count
+      if (temp > 0) {
+        posts.vote_count -= 1;
+        console.log("DOWN", temp);
+      }
+    };
   }
-}
-
-vm.createPost = function () {
-  let tempObj = {
-    title: vm.postObj.title,
-    body: vm.postObj.body,
-    author: vm.postObj.author,
-    image_url: vm.postObj.image_url,
-    vote_count: 0,
-    created_at: moment().calendar()
-  }
-
-  blogService.sendForm(tempObj).then(function (data){
-    console.log("DATA after new Form", data);
-    vm.blogDb.push(tempObj)
-    delete vm.postObj;
-  })
-
-  vm.toggleComment = function() {
-    vm.commentsVisible = !vm.commentsVisible;
-  };
-
-}
-
-vm.countVotesUp = function (posts){
-  let temp = posts.votes
-   posts.votes += 1
-   console.log("UP",temp);
-
-}
-vm.countVotesDown = function (posts){
-  let temp = posts.votes
-  if(temp > 0){
-    posts.votes -= 1;
-   console.log("DOWN",temp);
- }
-
-}
-
-}
-
 }());
--- app/public/js/blog.services.js
+++ app/public/js/blog.services.js
@@ -8,20 +8,29 @@
     function service ($http) {
       this.getBlog = function (){
         return $http.get('/api/posts').then(function (response) {
-          console.log("THIS",response);
+          console.log("JUST A GET REQUEST: ",response);
           return response.data
         })
       }
       this.sendForm = function (tempObj) {
         return $http.post('/api/posts', tempObj).then(function (response){
-          console.log("SUBFORM RES", response);
+          console.log("SUBMITTED FORM RESPONSE: ", response);
           return response.data
         })
       }
-
+      this.countVotesUp = function (posts){
+        console.log("POSTS ON UP COUNT IN SERVICE", posts);
+        return $http.post('/api/posts', posts).then(function (response){
+          console.log("count votes up RES", response);
+          return response.data
+      })
       }
-
-
+      this.deletePost = function (posts){
+        return $http.delete('/:id', posts).then(function (response){
+          console.log("DELETED POST OBJ", response);
+        })
+      }
+}
 
 
 
